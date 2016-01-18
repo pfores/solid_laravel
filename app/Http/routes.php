ada3ed1b01b40728b30aa3dc61cb7851d5929193
pfores <paufores@iesebre.com>
2016-01-18 16:27:40 +0100

    Tot Ok de moment

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,15 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
