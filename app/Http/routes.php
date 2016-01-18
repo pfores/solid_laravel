2e08d9b3c79524d7cc56892d1b907d0fce45b5d0
pfores <paufores@iesebre.com>
2016-01-18 17:15:14 +0100

    Error /invoices solucionat. Tot OK

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
     //
-    Route::get('/Invoice', 'InvoiceController@index');
+    Route::get('/invoices', 'InvoicesController@index');
 
 });
 
