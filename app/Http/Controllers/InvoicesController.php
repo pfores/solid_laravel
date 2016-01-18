cc09446817f1f69c7ce787d93cfa9dd282025556
pfores <paufores@iesebre.com>
2016-01-18 17:31:44 +0100

    Modificant InvoicesController

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -6,12 +6,35 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class InvoicesController extends Controller
 {
     //
     public function index()
     {
-        return view('invoices');
+
+        if( !Auth::check() ) {
+            return "Forbidden!";
+        }
+
+        $database_invoices = $this->getAllInvoicesFromDatabase();
+
+        $invoices = $this->transform($database_invoices);
+
+        return view('invoices',compact('invoices');
+
+//        $data['invoices'] = $formatted_invoices;
+//        return view('invoices',$data);
+
+    }
+
+    private function getAllInvoicesFromDatabase()
+    {
+        return ;
+    }
+
+    private function transform($database_invoices)
+    {
     }
 }
--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('htmlheader_title')
-	Home
+	Invoices
 @endsection
 
 
@@ -27,11 +27,11 @@
 					<div class="box-body table-responsive no-padding">
 						<table class="table table-hover">
 							<tr>
-								<th>ID</th>
-								<th>User</th>
-								<th>Date</th>
-								<th>Status</th>
-								<th>Reason</th>
+								<th>Id</th>
+								<th>Name</th>
+								<th>Created at</th>
+								<th>Updated at</th>
+								<th>Total</th>
 							</tr>
 							<tr>
 								<td>183</td>
@@ -40,27 +40,7 @@
 								<td><span class="label label-success">Approved</span></td>
 								<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
 							</tr>
-							<tr>
-								<td>219</td>
-								<td>Alexander Pierce</td>
-								<td>11-7-2014</td>
-								<td><span class="label label-warning">Pending</span></td>
-								<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-							</tr>
-							<tr>
-								<td>657</td>
-								<td>Bob Doe</td>
-								<td>11-7-2014</td>
-								<td><span class="label label-primary">Approved</span></td>
-								<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-							</tr>
-							<tr>
-								<td>175</td>
-								<td>Mike Doe</td>
-								<td>11-7-2014</td>
-								<td><span class="label label-danger">Denied</span></td>
-								<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-							</tr>
+
 						</table>
 					</div>
 					<!-- /.box-body -->
